6d9d123511724e4c2c93c3285f29891a51a0ba85
Surya Sankar <suryashankar.m@gmail.com>
2024-12-01 16:52:01 +0530

    More changes

--- src/content/utils/dom_utils.js
+++ src/content/utils/dom_utils.js
@@ -4,8 +4,10 @@ export const extractQuestionsForChatGPT = () => {
     articles.forEach(article=>{
         const articleType = article.children[0].textContent;
         if(articleType=='You said:'){
-        const userQuestion = article.getElementsByClassName("whitespace-pre-wrap")[0];
-        userQuestions.push(userQuestion);
+            const userQuestion = article.getElementsByClassName("whitespace-pre-wrap")[0];
+            if(userQuestion){
+                userQuestions.push(userQuestion);
+            }
         }
     });
     return userQuestions;
@@ -13,15 +15,15 @@ export const extractQuestionsForChatGPT = () => {
 
 export const extractQuestionsForClaudeAI = () => {
     const elements = document.getElementsByClassName("font-user-message");
-    return Array.from(elements).map(elem=>elem.children[0]).filter(elem=>elem.nodeName=='P');
+    return Array.from(elements).filter(elem=>elem).map(elem=>elem.children[0]).filter(elem=>elem.nodeName=='P');
 }
 
 export const extractQuestionsForPerplexityAI = () => {
     const elements = document.getElementsByClassName("group/query");
-    return Array.from(elements);
+    return Array.from(elements).filter(elem=>elem);
 }
 
 export const extractQuestionsForGemini = () => {
     const elements = document.getElementsByClassName("query-text");
-    return Array.from(elements).map(elem=>elem.children[0]);
+    return Array.from(elements).filter(elem=>elem).map(elem=>elem.children[0]);
 }
